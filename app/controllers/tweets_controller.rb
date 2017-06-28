7be889940bf44757cb7071b4803696108c21f282
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-06-28 14:42:30 +0900

    edit controller
    edit controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,17 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
 end
