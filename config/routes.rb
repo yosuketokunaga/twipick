f6fa6e863c07d6e1def712fd9dc8e7ed0d9fe0f1
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-07-09 12:47:39 +0900

    root画面を設定する。
    サービスにアクセスした際最初に一覧画面にたどり着くよう記述する。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
+  root 'tweets#index'
+  get  'tweets' => 'tweets#index'
+  get  'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
 end
