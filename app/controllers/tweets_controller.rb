e1df16f24ad8684b29db449e0bb851331fe0fc40
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-07-19 11:09:38 +0900

    edit controller
    updateとeditを追加する。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,6 +18,17 @@ class TweetsController < ApplicationController
     tweet.destroy if tweet.user_id == current_user.id
   end
 
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
 
   def tweet_params
