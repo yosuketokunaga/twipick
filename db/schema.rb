07e804a8740b452962feb7607f7fca5f3949374a
yosuketokunaga <tokunagayousuke@tokunaga-no-MacBook-Air.local>
2017-08-02 12:15:19 +0900

    コメントテーブルの作成
    コメント機能を作成するにあたりコメントテーブルを作成する。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719013927) do
+ActiveRecord::Schema.define(version: 20170802025126) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "tweet_id",   limit: 4
+    t.text     "text",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text     "text",       limit: 65535
